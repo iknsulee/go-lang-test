18855a933f216f3ddb23f2a673404c31ff66e9a0
keunsulee <i.knsulee@gmail.com>
2023-07-23 22:40:34 +0900

    add postgres sample code

--- kslee-test/kslee/http/http.go
+++ kslee-test/kslee/http/http.go
@@ -57,6 +57,32 @@ func PostHttp(token string, url string, reqBody string) (int, string) {
 	return httpResponse.StatusCode, httpResponseString
 }
 
+func PatchHttp(token string, url string, reqBody string) (int, string) {
+	req, err := http.NewRequest("PATCH", url, bytes.NewBuffer([]byte(reqBody)))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Content-Type", "application/json")
+
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
+	}
+	client := &http.Client{Transport: transCfg}
+
+	// Client 객체에서 Request 실행
+	httpResponse, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer httpResponse.Body.Close()
+
+	var bodyBytes, _ = io.ReadAll(httpResponse.Body)
+	var httpResponseString = string(bodyBytes) //바이트를 문자열로
+	//return httpResponse, bodyBytes, httpResponseString
+	return httpResponse.StatusCode, httpResponseString
+}
+
 func DeleteHttp(token string, url string, reqBody string) (int, string) {
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
